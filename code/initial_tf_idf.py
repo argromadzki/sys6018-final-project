7888f1f0869e57d4385c775d923dbadc34e59a84
argromadzki <41020899+argromadzki@users.noreply.github.com>
2018-12-07 18:09:40 -0500

    Relative paths in read_files and filled in tf-idf

--- code/initial_tf_idf.py
+++ code/initial_tf_idf.py
@@ -12,10 +12,14 @@ os.chdir("..//..//data")
 
 
 # read in data, ensuring that we have separate sets for queries and passages
-train_q = pd.read_csv("train_q.csv")
-train_p = pd.read_csv("train_p.csv")
-test_q = pd.read_csv("test_q.csv")
-test_p = pd.read_csv("test_p.csv")
+train_q = pd.read_table("queries.train.tsv")
+all_passages = pd.read_table("collection.tsv")
+test_q = pd.read_table("queries.dev.tsv")
+
+# clean columns
+train_q.columns = ['qid', 'text']
+test_q.columns = ['qid', 'text']
+all_passages.columns = ['pid', 'text']
 
 ''' Each Query and Each Passage should be a separate document
     ... Ought to have two seprate corpora -- one for queries and one for passages
@@ -36,12 +40,12 @@ train_q['tokens']=[[w for w in tokens if w.isalpha()] for tokens in train_q['tok
 train_q['newtext'] = train_q['tokens'].apply(lambda x: " ".join(x))
 
 # training set corpus creation: passage
-train_p['text'] = train_p['text'].apply(str.lower)
-train_p['tokens']=train_p['text'].apply(nltk.word_tokenize)
-train_p['tokens']=[[porter.stem(x) for x in tokens] for tokens in train_p['tokens']]
-train_p['tokens']=[[w for w in tokens if not w in stop_words] for tokens in train_p['tokens']]
-train_p['tokens']=[[w for w in tokens if w.isalpha()] for tokens in train_p['tokens']]
-train_p['newtext'] = train_p['tokens'].apply(lambda x: " ".join(x))
+all_passages['text'] = all_passages['text'].apply(str.lower)
+all_passages['tokens']=all_passages['text'].apply(nltk.word_tokenize)
+all_passages['tokens']=[[porter.stem(x) for x in tokens] for tokens in all_passages['tokens']]
+all_passages['tokens']=[[w for w in tokens if not w in stop_words] for tokens in all_passages['tokens']]
+all_passages['tokens']=[[w for w in tokens if w.isalpha()] for tokens in all_passages['tokens']]
+all_passages['newtext'] = all_passages['tokens'].apply(lambda x: " ".join(x))
 
 # testing set corpus creation: query
 test_q['text'] = test_q['text'].apply(str.lower)
@@ -51,21 +55,12 @@ test_q['tokens']=[[w for w in tokens if not w in stop_words] for tokens in test_
 test_q['tokens']=[[w for w in tokens if w.isalpha()] for tokens in test_q['tokens']]
 test_q['newtext'] = test_q['tokens'].apply(lambda x: " ".join(x))
 
-# testing set corpus creation: passage
-test_p['text'] = test_p['text'].apply(str.lower)
-test_p['tokens']=test_p['text'].apply(nltk.word_tokenize)
-test_p['tokens']=[[porter.stem(x) for x in tokens] for tokens in test_p['tokens']] # triggers a bug in nltk 3.2
-test_p['tokens']=[[w for w in tokens if not w in stop_words] for tokens in test_p['tokens']]
-test_p['tokens']=[[w for w in tokens if w.isalpha()] for tokens in test_p['tokens']]
-test_p['newtext'] = test_p['tokens'].apply(lambda x: " ".join(x))
-
 
 # write everything to csv's using pd.write_csv('FileName.csv')
-train_q.to_csv("train_query_corpus.csv")
-train_p.to_csv("train_passage_corpus.csv")
 
+train_q.to_csv("train_query_corpus.csv")
+all_passages.to_csv("all_passages_corpus.csv")
 test_q.to_csv("test_query_corpus.csv")
-test_p.to_csv("test_passage_corpus.csv")
 
 ''' do we need to read in these files or csvs?  
     ... we did before, but i think it was an unnecessary step
@@ -75,22 +70,17 @@ test_p.to_csv("test_passage_corpus.csv")
 vectorizer_min = slr.feature_extraction.text.CountVectorizer(min_df=.0005)
 
 train_q_tdm_reduced = vectorizer_min.fit_transform(train_q['newtext'].astype('U'))
-train_p_tdm_reduced = vectorizer_min.fit_transform(train_p['newtext'].astype('U'))
-
+passages_tdm_reduced = vectorizer_min.fit_transform(all_passages['newtext'].astype('U'))
 test_q_tdm_reduced = vectorizer_min.transform(test_q['newtext'].astype('U'))
-test_p_tdm_reduced = vectorizer_min.transform(test_p['newtext'].astype('U'))
 
 # TFIDF transform
 transformer = slr.feature_extraction.text.TfidfTransformer(smooth_idf=False)
 
 train_q_tfidf = transformer.fit_transform(train_q_tdm_reduced)
-train_p_tfidf = transformer.fit_transform(train_p_tdm_reduced)
+passages_tfidf = transformer.fit_transform(passages_tdm_reduced)
 
 test_q_tfidf = transformer.fit_transform(test_q_tdm_reduced)
-test_p_tfidf = transformer.fit_transform(test_p_tdm_reduced)
 
 sp.sparse.save_npz("Training_TF_IDF_Q.npz", train_q_tfidf)
-sp.sparse.save_npz("Training_TF_IDF_P.npz", train_p_tfidf)
-
+sp.sparse.save_npz("Passages_TF_IDF.npz", passages_tfidf)
 sp.sparse.save_npz("Testing_TF_IDF_Q.npz", test_q_tfidf)
-sp.sparse.save_npz("Testing_TF_IDF_P.npz", test_p_tfidf)
