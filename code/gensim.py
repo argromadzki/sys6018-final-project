eee0225584014835f09c77cbac8a8aec10b37110
SungMin42 <michael.sm.yang@gmail.com>
2018-12-07 19:33:33 -0500

    gensim

--- code/gensim.py
+++ code/gensim.py
@@ -1,4 +1,16 @@
+#https://www.kaggle.com/marijakekic/cnn-in-keras-with-pretrained-word2vec-weights
 # Gensin Word Embeddings
 import gensim
-qrels_train
-model = gensim.models.Word2Vec.load_word2vec_format()
+import pandas as pd
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+
+
+queries_train = pd.read_table('data\queries.train.tsv', names = ['pid', 'text'])
+queries_train_text = queries_train['text']
+word2vec_pre = gensim.models.KeyedVectors.load_word2vec_format('pretrained word embeddings\GoogleNews-vectors-negative300.bin', binary = True)
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
